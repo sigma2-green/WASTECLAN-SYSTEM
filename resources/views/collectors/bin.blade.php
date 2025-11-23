4d1ba80dc01dfe544972ba353d8fed402e2d840e
Adrian <adrianshisia@gmail.com>
2025-11-24 00:03:15 +0300

    collectors

--- resources/views/collectors/bin.blade.php
+++ resources/views/collectors/bin.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+{{-- resources/views/collectors/bin.blade.php --}}
+<h2>Bins</h2>
+<table class="table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($bins as $bin)
+            <tr>
+                <td>{{ $bin->id }}</td>
+                <td>{{ $bin->name }}</td>
+                <td>{{ $bin->status }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
--- resources/views/collectors/routes.blade.php
+++ resources/views/collectors/routes.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+{{-- resources/views/collectors/routes.blade.php --}}
+<h2>Routes</h2>
+<ul>
+    @foreach($routes as $route)
+        <li>{{ $route->name }} - {{ $route->description }}</li>
+    @endforeach
+</ul>
