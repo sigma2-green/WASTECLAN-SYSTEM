a1944a784a3ba5c06ed47541171aba8e0b26f483
chogocorban <corbanchogo1@gmail.com>
2025-11-24 03:30:11 +0300

    collect and sorting views in residents section

--- resources/views/residents/sorting.blade.php
+++ resources/views/residents/sorting.blade.php
@@ -1,8 +1,28 @@
 @extends('layout.app')
 
 @section('content')
-<!--SET TO SHOW ALL SYSTEM GENERATED SORTING GUIDES
-    
+<div class="container mt-4">
+    <h2>Sorting Guides</h2>
+    <div class="row">
+        @forelse($guides as $guide)
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $guide->title }}</h5>
+                        <p class="card-text">{{ $guide->description }}</p>
+                        <ul>
+                            @foreach($guide->steps as $step)
+                                <li>{{ $step }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <p>No sorting guides available.</p>
+        @endforelse
+    </div>
+</div>
 @endsection
 
 
